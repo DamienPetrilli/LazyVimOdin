0a3c9cab510c6ad23f05fc3fa9fbba82333fe752
Damien Petrilli <DamienPetrilli@users.noreply.github.com>
2024-08-23 13:50:04 +0200

    Update odin.lua

--- lua/plugins/odin.lua
+++ lua/plugins/odin.lua
@@ -1,35 +1,35 @@
 -- File: ~/.config/nvim/lua/plugins/odin.lua
 
 return {
-  -- Treesitter configuration (unchanged)
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "odin" })
-      end
-    end,
-  },
+	-- Treesitter configuration (unchanged)
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			if type(opts.ensure_installed) == "table" then
+				vim.list_extend(opts.ensure_installed, { "odin" })
+			end
+		end,
+	},
 
-  -- Updated LSP configuration
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        ols = {
+	-- Updated LSP configuration
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				ols = {
 					mason = false,
-        },
-      },
-      setup = {
+				},
+			},
+			setup = {
 				ols = function(_, opts)
-        require("lspconfig").ols.setup(vim.tbl_deep_extend("force", {
-          on_attach = function(client, bufnr)
-            print("OLS server capabilities:")
-            print(vim.inspect(client.server_capabilities))
-          end,
-        }, opts))
+					require("lspconfig").ols.setup(vim.tbl_deep_extend("force", {
+						on_attach = function(client, bufnr)
+							print("OLS server capabilities:")
+							print(vim.inspect(client.server_capabilities))
+						end,
+					}, opts))
 				end,
-      },
-    },
-  },
+			},
+		},
+	},
 }
